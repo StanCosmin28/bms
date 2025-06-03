23d8a05585b585ed3964b55063d293cac540c0df
Stan Cosmin <stancosminioan@gmail.com>
2025-06-03 09:20:08 +0300

    more media

--- src/Posts/techGridData.js
+++ src/Posts/techGridData.js
@@ -4,16 +4,16 @@ const images = [
     src: `${URL}/grid-img-1.webp`,
   },
   {
-    src: `${URL}/grid-img-3.webp`,
+    src: `${URL}/grid-img-4.webp`,
   },
   {
     src: `${URL}/grid-img-2.webp`,
   },
   {
-    src: "https://images.unsplash.com/photo-1518770660439-4636190af475?w=500&auto=format&fit=crop&q=60",
+    src: `${URL}/grid-img-3.webp`,
   },
   {
-    src: `${URL}/grid-img-4.webp`,
+    src: "",
     title:
       "Suntem o gașcă de entuziaști tech, gata să-ți aducem cele mai tari noutăți din lumea BMS.",
     description: "Info fresh | Vibe profi | Strop de FUN",
